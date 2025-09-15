54a384aafa8ef3205e25ee3d30a3b811ab69308e
sharmila-2406 <620kumarisharmila@gmail.com>
2025-09-15 18:24:52 +0530

    conditional statement

---
+++ 04_Javascript/01_Core JS/02_js fundamentals  practice Question/09_PQ.js
@@ -0,0 +1,11 @@
+// What are conditional statements? Give an example using if...else.
+
+// Conditional statements:- Conditional statements allow your program to make decisions based on certain conditions.
+// They execute a block of code only if a condition is true.
+let age = 18;
+
+if (age >= 18){
+    console.log("you are  eligible for vote");
+}else {
+    console.log("you are not eligible fr vote");
+}
