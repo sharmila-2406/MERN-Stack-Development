205158c0aaf09ab8be3280cdd33bb2fda045a6ec
sharmila-2406 <620kumarisharmila@gmail.com>
2025-09-15 15:06:27 +0530

    Variable practice  Question

---
+++ 04_Javascript/01_Core JS/02_js fundamentals  practice Question/01_PQ.js
@@ -0,0 +1,10 @@
+// Declare a variable name and assign your name to it. Print it in the console.
+
+var myName = "Sharmila";
+console.log(myName);
+
+let myAge = 23;
+console.log(myAge);
+
+const myclass = "MCA";
+console.log(myclass);
