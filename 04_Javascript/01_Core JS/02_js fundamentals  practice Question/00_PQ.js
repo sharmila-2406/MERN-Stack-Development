e890eccf291926c19bebd96b2db9629c9b571a53
sharmila-2406 <620kumarisharmila@gmail.com>
2025-09-15 15:51:39 +0530

    js intro

--- 04_Javascript/01_Core JS/02_js fundamentals  practice Question/00_PQ.js
+++ 04_Javascript/01_Core JS/02_js fundamentals  practice Question/00_PQ.js
@@ -1,22 +1,22 @@
 // What is JavaScript and where is it used?
 
-JavaScript is a high-level, interpreted programming language mainly used to make web pages interactive and dynamic.
-It runs directly in the web browser of the user.
-It was originally created to add behavior to web pages, like handling button clicks, form submissions, animations, etc.
+// JavaScript is a high-level, interpreted programming language mainly used to make web pages interactive and dynamic.
+// It runs directly in the web browser of the user.
+// It was originally created to add behavior to web pages, like handling button clicks, form submissions, animations, etc.
 
-JavaScript is used in many areas:-
+// JavaScript is used in many areas:-
 
-Web development (Frontend)
- To make websites interactive (like buttons, menus, forms, image sliders)
+// Web development (Frontend)
+//  To make websites interactive (like buttons, menus, forms, image sliders)
 
-Web development (Backend)
-  Using Node.js, JavaScript can run on servers
+// Web development (Backend)
+//   Using Node.js, JavaScript can run on servers
 
-Mobile app development:-
-  Using frameworks like React Native
+// Mobile app development:-
+//   Using frameworks like React Native
 
-Game development:-
-  To create browser-based games
+// Game development:-
+//   To create browser-based games
 
-Desktop app development:-
-  Using Electron to make cross-platform desktop apps
+// Desktop app development:-
+//   Using Electron to make cross-platform desktop apps
