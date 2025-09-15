d211c79a2ef438a8e3879e32c7bb83354ee99ff7
sharmila-2406 <620kumarisharmila@gmail.com>
2025-09-15 16:01:07 +0530

    null & undefined

---
+++ 04_Javascript/01_Core JS/02_js fundamentals  practice Question/08_PQ.js
@@ -0,0 +1,9 @@
+// What is the difference between null and undefined?
+
+// null :- A variable is explicitly assigned with "no value" by the programmer.
+let a = null;
+console.log(a);
+
+//undefined:-  A variable has been declared but not assigned any value yet.
+let b;
+console.log(b);
