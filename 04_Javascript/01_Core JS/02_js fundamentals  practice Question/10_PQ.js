7b1948cd65b0c4adefdf6cea23ffe0d115404333
sharmila-2406 <620kumarisharmila@gmail.com>
2025-09-19 06:15:52 +0530

    tupe casting

---
+++ 04_Javascript/01_Core JS/02_js fundamentals  practice Question/10_PQ.js
@@ -0,0 +1,23 @@
+// What are operators in JavaScript? Give examples.
+
+// Operators are symbols used to perform operations on values or variables.
+// They are essential for calculations, comparisons, and logical operations in JavaScript.
+//           Type of Operators:-
+// 1>.  Arithmetic Operators - For math operations.
+            let x = 32;
+            let y = 32;
+            sum = 32+32;
+            console.log(sum);
+// 2>.  Assignment Operators – Assign values to variables.
+            x += 4;
+            console.log(x);
+// 3>.  Comparison Operators – Compare values
+            result = x == y;
+            great = x > y;
+            less = x < y;
+            console.log(result);
+            console.log(great);
+            console.log(less);
+// 4>.  Logical Operators – Combine conditions
+// 5>.
+// 6>.
---
+++ 04_Javascript/01_Core JS/type casting/PQ_1.js
@@ -0,0 +1,2 @@
+let result = 10 + "5";
+console.log(typeof result);
